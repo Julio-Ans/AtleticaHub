1b3abd902a55fb6a949f284ed863ff8b32388c43
julio <julio.mendes@fatec.sp.gov.br>
2025-06-08 23:00:28 -0300

    loja 2.0

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 async function connectMongoDB() {
   await mongoose.connect(process.env.MONGODB_URI);
-  console.log('✅ MongoDB conectado com sucesso');
 }
 
 module.exports = connectMongoDB;
